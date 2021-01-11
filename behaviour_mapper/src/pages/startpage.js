3022c7d1c3e4e1c4a26d88e899e291700e378e7f
Sveinung <sveinung87@gmail.com>
2021-01-11 14:37:10 +0100

    startpage css 11/01

--- behaviour_mapper/src/pages/startpage.js
+++ behaviour_mapper/src/pages/startpage.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import './startpage.css';
 
 
+// function change_visibility() {
+//   if (new_project) {
+//     new_project = false
+//   }
+//   else {
+//     new_project = true
+//   }
+// }
 
 function Startpage() {
+  const [new_project, setNewProject] = useState(false)
+
+  const hideSection = () => setNewProject(!new_project)
+
     return (
       <div className="startpage">
         <ul id='start-menu'>
-        <Link to="/mapping"><li>New Project</li></Link>
+        <li onClick={hideSection}>New Project</li>
           <li className='start-menu-li'>Load Project</li>
           <li>User Profile</li>
         </ul>
-        <ul id='load-map'>
+        <ul id={new_project ? 'load-map' : 'invisible'}>
           <li>Load map from file</li>
           <li>Use web-map</li>
         </ul>
