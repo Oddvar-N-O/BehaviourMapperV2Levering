9f3a650097987b5d00f52ef54ad8f6305984ffa8
Torcar <tor.hakon@laposte.net>
2021-01-13 13:33:57 +0100

    add branch?

--- flask-api/app.py
+++ flask-api/app.py
@@ -7,3 +7,16 @@ CORS(app)
 @app.route('/hello')
 def say_hello_world():
     return {'result': "Koplingen mellom flask og react fungerer"}
+
+@app.route('/')
+def say_hello_world():
+    return {'result': "Legg til innlogging her"}
+
+@app.route('/upload')
+def say_hello_world():
+    return {'result': "Her skal vi upploade kartet"}
+
+
+@app.route('/selectImage')
+def say_hello_world():
+    return {'result': "Open Street Map"}
