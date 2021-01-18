77413dd1e337de4494d3eda2ac63724e70b5d7a9
Sveinung <sveinung87@gmail.com>
2021-01-18 13:33:13 +0100

    bugs

--- behaviour_mapper/src/pages/UploadMap.js
+++ behaviour_mapper/src/pages/UploadMap.js
@@ -1,22 +1,32 @@
-import React from "react"
+import React, {Component} from "react"
 import "./UploadMap.css"
 import * as AiIcons from "react-icons/ai";
 import { Link } from 'react-router-dom';
 
-function UploadMap() {
-    return(
-        <div className="upload-map">
-            <div id="upload-box">
-                <Link to="/startpage" className="close-icon">
-                    <AiIcons.AiOutlineClose />
-                </Link>
-                <form>
-                    <input type="file" /> <br></br>
-                    <button>Upload</button>
-                </form>
+class UploadMap extends React.Component() {
+    constructor() {
+        super()
+        this.state = {
+            name: "",
+            selectedFile: null,
+        }
+    }
+    render() {
+        return(
+            <div className="upload-map">
+                <div id="upload-box">
+                    <Link to="/startpage" className="close-icon">
+                        <AiIcons.AiOutlineClose />
+                    </Link>
+                    <form>
+                        <input type="file" /> <br></br>
+                        
+                    </form>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
-export default UploadMap
+
+// export default UploadMap
