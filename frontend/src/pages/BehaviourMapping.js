29273db04dbef2172a23ceac700f3bd5b1c952e9
Torcar <tor.hakon@laposte.net>
2021-01-22 15:06:38 +0100

    Nu borde det funka

--- frontend/src/pages/BehaviourMapping.js
+++ frontend/src/pages/BehaviourMapping.js
@@ -23,7 +23,7 @@ function BehaviourMapping() {
       <div id='maincont'>
           <div class='divImgContainer' onClick={e => showMarker(e)}>
               <img alt="" id="myimage" height="500px" width="500px" src="https://www.talkwalker.com/images/2020/blog-headers/image-analysis.png" />
-              <div id='divMark' > <img width='100px' height='100px' id='myimage' src='https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg'></img></div>
+              <div id='divMark' > <img alt="" width='100px' height='100px' id='myimage' src='https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg'></img></div>
           </div>
           
       </div>
