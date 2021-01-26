41c9124e276f74c64d7b1c216f8ddaf1dc006aad
Torcar <tor.hakon@laposte.net>
2021-01-26 14:09:34 +0100

    sender iconene til databasen

--- frontend/src/pages/BehaviourMapping.js
+++ frontend/src/pages/BehaviourMapping.js
@@ -3,6 +3,9 @@ import React from 'react';
 // import Sidebar from '../components/sidebar'
 import './BehaviourMapping.css'
 
+// rettningen, xogykoordinat, tid, icon
+// event tilknyttet til figur 
+
 class BehaviourMapping extends React.Component {
   constructor() { //props
       super() //props
@@ -22,6 +25,7 @@ class BehaviourMapping extends React.Component {
         ourIconCoord: {
           x: 0,
           y: 0,
+          degree: 0,
         },
         ourMouseCoord: {
           x: 0,
@@ -33,23 +37,40 @@ class BehaviourMapping extends React.Component {
       };
   }
 
+  // set state to change the uri
+  sendDatabaseEvent() {
+      // rettningen, xogykoordinat, tid, icon
+      const data = new FormData();
+      const coordinates = [this.state.ourIconCoord.x-25, this.state.ourIconCoord.y-25];
+      data.append('direction', this.state.ourIconCoord.degree);
+      data.append('center_coordinate', coordinates);
+      data.append('created', new Date());
+
+      console.log(data);
+
+      fetch('http://localhost:5000/addevent', {
+      method: 'POST',
+      body: data,
+      }).then((response) => {
+        console.log(response);
+      });
+    }
+
   showMarker(event) {
     var mark = document.getElementById('icon');
     mark.style.top =  (event.clientY)+'px';
     mark.style.left = (event.clientX) +'px';
   }
 
-  // bruk disse 2 funksjonen for å legge til
-  // items som props
-
-  // her forandrer vi bildet
   changeIcon(icNum) {
-    console.log("Test");
-    console.log(icNum);
+    // choose new icon
     this.setState({
       imgIcon: icNum
     });
+    // hide old icon
     this.hideIcon();
+    // send old icon to db
+    this.sendDatabaseEvent()
   }
 
   hideIcon() {
@@ -57,22 +78,24 @@ class BehaviourMapping extends React.Component {
     if (icon != null) {
       icon.style.display = 'none';
     }
-    this.state.actionID = 0;
+    // this.state.actionID = 0;
+    this.setState({
+      actionID: 0,
+    });
   }
 
   takeAction(event) {
     if (this.state.actionID === 0) {
       this.showIcon(event);
-      this.state.actionID = 1;
+      // this.state.actionID = 1;
+      this.setState({
+        actionID: 1,
+      });
     } else {
       this.pointIcon();
     }
   }
 
-  /* I have already figured out that by drawing a line between Bob and
-  the mouse and by finding the angle of this line, i can find out what
-  the angle is, that Bob needs to face in order to 'point' towards the
-  mouse. However I still don't know how to accomplish this task. */
   pointIcon() {
 
     var degreerot = Math.atan2(
@@ -83,10 +106,22 @@ class BehaviourMapping extends React.Component {
     var degrees = degreerot*180/Math.PI;
     var round_degree = Math.round(degrees);
     var string_degree = 'rotate(' + round_degree.toString() +'deg)';
-    console.log("degROTATE: " + string_degree);
-
+    // console.log("degROTATE: " + string_degree);
+    // this.state.ourIconCoord.degree = string_degree;
+    this.setState({
+      ourIconCoord: {
+        x: this.state.ourIconCoord.x,
+        y: this.state.ourIconCoord.y,
+        degree: string_degree
+      }
+    });
+    
+    console.log('degree: ' + string_degree);
+    console.log('our degree: ' + this.state.ourIconCoord.degree);
+    // point icon with css
     var img = document.getElementById(this.state.ourIconID.toString());
     img.style.transform = string_degree;
+
   }
 
   showIcon(event) {
@@ -98,10 +133,15 @@ class BehaviourMapping extends React.Component {
     img.classList.add('icon');
     img.setAttribute('id', this.state.newIconID.toString());
 
-    this.state.ourIconID = this.state.newIconID;
+    // this.state.ourIconID = this.state.newIconID;
+    this.setState({
+      ourIconID: this.state.newIconID
+    });
     // console.log("OurICONID: " + this.state.ourIconID);
-
-    this.state.newIconID += 1;
+    this.setState({
+      newIconID: this.state.newIconID + 1
+    });
+    // this.state.newIconID += 1;
     img.addEventListener('click', function() {
       console.log(img.getAttribute('id'));
     });
@@ -117,26 +157,33 @@ class BehaviourMapping extends React.Component {
     var y = event.clientY;
     // console.log("Placement: Left? : " + x + " ; Top? : " + y + ".");
     
-    this.state.ourIconCoord.x = x + 25;
-    this.state.ourIconCoord.y = y + 25;
-
-    // make rotate
+    this.setState({
+      ourIconCoord: {
+        x: x + 25,
+        y: y + 25,
+      }
+    });
   }
 
   updateCoord(event) {
-      this.state.ourMouseCoord.x = event.clientX;
-      this.state.ourMouseCoord.y = event.clientY;
+    this.setState({
+      ourMouseCoord: {
+        x: event.clientX,
+        y: event.clientY,
+      }
+    });
       // console.log("Mouse: x : " + event.clientX + " ; y : " + event.clientY + ".");
   }
 
-  // onmousemove skulle la bildene bli dratt av musen
-  // trykk for å velge ikon
-  // Trykk på bildet 1 for å plassere
-  // trykk på bildet 2 for å plassere
-  // trykk på icon 1 for å bli kvitt bildet
-
-  showAll() {
+  print() {
     console.log('Print all');
+    console.log('currID: ' + this.state.newIconID);
+    var icon;
+    for (var i=0; i<this.state.newIconID; i++) {
+      console.log('id ' + i);
+      icon = document.getElementById(i.toString());
+      icon.style.display = 'block';
+    }
   }
 
   render() {
@@ -155,12 +202,11 @@ class BehaviourMapping extends React.Component {
         <img alt="" onMouseMove={e => this.updateCoord(e)} onClick={e => this.takeAction(e)} id='backgroundImage' height="500px" width="500px" src={this.state.background} />
         <div id="iconContainer">
         </div>
-        <button onClick={() => this.showAll()}>SHOW ALL</button>
+        <button onClick={() => this.showAll()}>Show</button>
       </div>
     );
-  } //  onMouseMove={e => this.rotBigImg(e)}
-} // <button>You are now: {this.state.actionText[this.state.actionID]}</button>
-// <button onClick={() => this.insertIntoIcons("Yo")}>AppendIconToList</button>
-  
+  }
+}
+
 export default BehaviourMapping;
 
