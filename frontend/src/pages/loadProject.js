664a41b6814d9d2cba53b74dd594710f1acba4ad
OddvarNO <54265658+OddvarNO@users.noreply.github.com>
2021-02-03 13:31:05 +0100

    loadProject page working

--- frontend/src/pages/loadProject.js
+++ frontend/src/pages/loadProject.js
@@ -5,11 +5,8 @@ import './loadProject.css'
 
 function LoadProject() {
   const [allProjects, setAllProjects] = useState('No projects found');
-  // const [allEvents, setallEvents] = useState('No event for this project')
-  const [currProjName, setCurrProjName] = useState(null);
-  const [currProjId, setcurrProjId] = useState(null);
-  const [currProjDesc, setcurrProjDesc] = useState(null);
-  const [currProjScrsh, setcurrProjScrsh] = useState(null);
+  const [allEvents, setallEvents] = useState('No event for this project')
+  const [currProj, setCurrProj] = useState({});
   const [showProjInfo, setshowProjInfo] = useState(false);
 
   useEffect(() => {
@@ -18,18 +15,30 @@ function LoadProject() {
     var fetchstring = `getproject?u_id=${userID}`
     fetch(fetchstring).then(res => res.json()).then(data => {
       setAllProjects(data);
-      fetchstring = `getproject?u_id=${userID}`
     });
   }, []);
+
+  useEffect(() => {
+    var fetchstring = `getevents?p_id=${currProj['id']}`
+    fetch(fetchstring).then(res => res.json()).then(data => {
+      setallEvents(data);
+    });
+  }, [currProj]);
 
   const getCurrProj = (index) => {
     for (var i = 0; i < allProjects.length; i++) {
       if (allProjects[i][1] === index) {
         setshowProjInfo(true);
-        setcurrProjId(allProjects[i][0]);
-        setCurrProjName(allProjects[i][1]);
-        setcurrProjDesc(allProjects[i][2]);
-        setcurrProjScrsh(allProjects[i][4]);
+        setCurrProj({
+          "id": allProjects[i][0], 
+          "name": allProjects[i][1],
+          "description": allProjects[i][2],
+          "map": allProjects[i][3],
+          "screenshot": allProjects[i][4],
+          "startdate": allProjects[i][5],
+          "enddate": allProjects[i][6],
+          "zoom": allProjects[i][7],
+          "u_id": allProjects[i][8]});      
       }
     }
   }
@@ -39,10 +48,16 @@ function LoadProject() {
           <div className="load-project-box">
             <SidebarLP  getCurrProj={getCurrProj} projects={allProjects} />
             <div className={showProjInfo ? "show-project-list" : "hide-project-list"}>
-              <h1>Description: {currProjDesc}</h1>
-              <p>Project id: {currProjId} Name: {currProjName} Screenshot: {currProjScrsh}</p>
-              <img alt={'Screenshot av kartet til '+ currProjName + '.'} id='opplastetKart' />
-              <button><Link></Link>Choose this project</button>
+              <h1>Description: {currProj["description"]}</h1>
+              <p>Project id: {currProj["id"]} Name: {currProj["name"]} Screenshot: {currProj["screenshot"]} Events: {allEvents}</p>
+              <img alt={'Screenshot av kartet til '+ currProj["name"] + '.'} id='opplastetKart' />
+              <button><Link to={{
+                        pathname: "/mapping",
+                        data: {
+                            project: currProj,
+                            events: {allEvents},
+                        }
+                        }}>Choose this project</Link></button>
             </div>
           </div>
         </div>
