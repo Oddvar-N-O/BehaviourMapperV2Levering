fcda6509d4448dcefbf8406995f3efaa3c36cf2b
OddvarNO <54265658+OddvarNO@users.noreply.github.com>
2021-02-04 15:42:36 +0100

    added test for sidebarLP

--- frontend/src/components/__tests__/testsidebarLP.js
+++ frontend/src/components/__tests__/testsidebarLP.js
@@ -1,9 +1,24 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import SidebarLP from '../sidebarLP';
+import React from 'react';
+import { shallow } from 'enzyme';
+import SidebarLP from '../sidebarLP';
 
-// // må testes på annen måte
-// it('renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<SidebarLP />, div);
-// });
+function smallfunc() {
+    console.log("getProj");
+}
+
+// må testes på annen måte
+it('shows a list of projects', () => {
+    const initialProps = {
+        projects: [[1, "prosjektnamn", "beskrivelse", "kartet", "screenshot", "1998-01-30 12:23:43", "1998-01-30 12:23:43", "zoom", 1],[2,"test","besk2"]],
+        getCurrProj: smallfunc,
+    }
+    const testlist = shallow(
+        <SidebarLP  {...initialProps}/>  
+  );
+  const sidebarComponents = testlist.find('li')
+  expect(sidebarComponents.length).toEqual(initialProps.projects.length);
+  sidebarComponents.forEach((sidebarComponent, index) => {
+    //   console.log(sidebarComponent.getElements()[0]['props']);
+      expect(sidebarComponent.prop('children')).toEqual(initialProps.projects[index][1]);
+  });
+});
