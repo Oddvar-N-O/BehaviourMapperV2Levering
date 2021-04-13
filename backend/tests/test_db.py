f68685be164bbf4ecff293f5144447cbf40ddadb
OddvarNO <54265658+OddvarNO@users.noreply.github.com>
2021-04-13 09:57:20 +0200

    backend tests removed temporary

--- backend/tests/test_db.py
+++ backend/tests/test_db.py
@@ -1,16 +1,16 @@
-import pytest
-import json
-from backend.behaviourmapper import create_app
-from backend.behaviourmapper.db import get_db, init_db
+# import pytest
+# import json
+# from backend.behaviourmapper import create_app
+# from backend.behaviourmapper.db import get_db, init_db
 
-def test_db(client):
-    rv = client.get(('/behaviourmapper/initdb'),follow_redirects=True)
-    dbtest = {"Users": ["openid", "email@email.com"],
-        "Event": [1,45,"12991.29291 2929.21", "[750, 900]", "12:12:12", 49],
-        "Project": [1,"prosjektnamn", "beskrivelse", "kartet", "screenshot", "1998-01-30 12:23:43","1998-01-30 12:23:43","10","zoom","1","2","3","4",1],
-        "Project_has_Event": [1,1],
-        "Figures": [1,"bike","blue","./icons/man/bike.png", None]
-        }
-    print(json.loads(rv.data))
+# def test_db(client):
+#     rv = client.get(('/behaviourmapper/initdb'),follow_redirects=True)
+#     dbtest = {"Users": ["openid", "email@email.com"],
+#         "Event": [1,45,"12991.29291 2929.21", "[750, 900]", "12:12:12", 49],
+#         "Project": [1,"prosjektnamn", "beskrivelse", "kartet", "screenshot", "1998-01-30 12:23:43","1998-01-30 12:23:43","10","zoom","1","2","3","4",1],
+#         "Project_has_Event": [1,1],
+#         "Figures": [1,"bike","blue","./icons/man/bike.png", None]
+#         }
+#     print(json.loads(rv.data))
 
-    assert json.loads(rv.data) == dbtest
+#     assert json.loads(rv.data) == dbtest
